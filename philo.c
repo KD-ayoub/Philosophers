089aad5e1b3e6d248e5647d236bfd45209dc172f
Ayoub Kadi <akadi@e3r11p12.1337.ma>
2022-06-19 17:57:48 +0100

    up

--- philo.c
+++ philo.c
@@ -6,17 +6,48 @@
 /*   By: akadi <akadi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 12:26:53 by akadi             #+#    #+#             */
-/*   Updated: 2022/06/14 13:11:14 by akadi            ###   ########.fr       */
+/*   Updated: 2022/06/19 17:41:21 by akadi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+long	get_current_time(void)
+{
+	struct timeval	t;
+	long			cur_time;
+
+	gettimeofday(&t, NULL);
+	cur_time = (t.tv_sec * 1000 + t.tv_usec / 1000);
+	return (cur_time);
+}
+
+void	get_arguments(info *arg, char **av, int ac)
+{
+	t_philo	*philo;
+	
+	philo = malloc(sizeof(t_philo) * ft_atoi(av[1])); 
+	arg->number_of_philos = ft_atoi(av[1]);
+	arg->time_to_die = ft_atoi(av[2]);
+	arg->time_to_eat = ft_atoi(av[3]);
+	arg->time_to_sleep = ft_atoi(av[4]);
+	if (ac == 6)
+	{
+		if (!(arg->n_of_philo_must_eat = ft_atoi(av[5])))
+			return ;
+	}
+	else
+		arg->n_of_philo_must_eat = -1;
+	
+}
 int	main(int ac, char **av)
 {
-	if (ac == 1)
+	info	arg;
+
+	if (check_error(ac, av) || ac == 1)
 		return (0);
-	if (check_error(ac, av))
-		return (0);
+	
+	get_arguments(&arg, av, ac);
+	
 }
 
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: akadi <akadi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 12:29:17 by akadi             #+#    #+#             */
-/*   Updated: 2022/06/16 15:37:04 by akadi            ###   ########.fr       */
+/*   Updated: 2022/06/19 16:44:02 by akadi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,34 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/time.h>
 #include <fcntl.h>
 #include <pthread.h>
 
+typedef struct argument
+{
+	int				number_of_philos;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				n_of_philo_must_eat;
+	int				dead;
+	long			time;
+	pthread_mutex_t	mutex;
+	pthread_mutex_t	*forks;
+
+}info;
+
+typedef struct s_philo
+{
+	int		num_philo;
+	int		num_eat;
+	long	last_meal;
+	info	*info;
+	pthread_mutex_t	*right;
+	pthread_mutex_t	*left;
+	pthread_t	thread;
+}t_philo;
 
 int	ft_strlen(char *str);
 int	ft_atoi(const char *str);
